ce3105db39fd468bb51c35c9c2bb4e5a3b5dbdfa
Ellienn Tatar <ellienntatar@outlook.com>
2022-12-06 17:01:15 -0800

    Add ChestUtil class
    The idea behind this is to have helper methods so they can be used both in a class for single chests and double chests

---
+++ src/main/java/dev/ellienntatar/inventory/ChestUtil.java
@@ -0,0 +1,37 @@
+package dev.ellienntatar.inventory;
+
+import java.util.List;
+import java.util.Map.Entry;
+
+import org.bukkit.Bukkit;
+import org.bukkit.Material;
+import org.bukkit.event.inventory.InventoryType;
+import org.bukkit.inventory.*;
+import org.bukkit.inventory.ItemStack;
+
+public class ChestUtil {
+    
+    private ChestUtil() {
+        // prevents instantiation
+    }
+
+    public static void outputContents(Inventory holderInventory, List<Entry<Material, Integer>> list) {
+        Inventory sortedInventory = Bukkit.createInventory(holderInventory.getHolder(), InventoryType.CHEST);
+        
+        for (Entry<Material, Integer> entry : list) {
+            int numItems = entry.getValue();
+            while (numItems > 0) {
+                ItemStack singleStack = new ItemStack(entry.getKey(), 1);
+                if (singleStack.getMaxStackSize() == 1) {
+                    sortedInventory.addItem(singleStack);    
+                    numItems -= 1;
+                } else {
+                    int currNum = numItems > 64 ? 64 : numItems;
+                    sortedInventory.addItem(new ItemStack(entry.getKey(), currNum));
+                    numItems -= currNum;
+                }
+            }
+        }
+        holderInventory = sortedInventory;
+    }
+}
