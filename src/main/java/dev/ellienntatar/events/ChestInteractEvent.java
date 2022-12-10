7aee70cb5a5f6aba3e425c6d9237ceae7cc28e39
Ellienn Tatar <ellienntatar@outlook.com>
2022-12-10 15:37:15 -0800

    Add ShulkerBox sortability

--- src/main/java/dev/ellienntatar/events/ChestInteractEvent.java
+++ src/main/java/dev/ellienntatar/events/ChestInteractEvent.java
@@ -5,9 +5,12 @@ import org.bukkit.event.*;
 import java.util.*;
 
 import org.bukkit.ChatColor;
+import org.bukkit.block.BlockState;
 import org.bukkit.block.Chest;
+import org.bukkit.block.ShulkerBox;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.inventory.Inventory;
 
 import dev.ellienntatar.inventory.InventoryUtil;
 import dev.ellienntatar.inventory.InventoryUtil.SortType;
@@ -22,13 +25,25 @@ public class ChestInteractEvent implements Listener {
     public void onPlayerInteract(PlayerInteractEvent event) {
         String playerName = event.getPlayer().getName();
         if (hasToggledSort.containsKey(playerName)) {
-            if (event.getClickedBlock().getState() instanceof Chest) {
-                SortType sortType = playerSortType.get(playerName);
-                Chest clickedChest = (Chest) event.getClickedBlock().getState();
-                Sortable sorter = InventoryUtil.getSorter(sortType, clickedChest.getInventory());
-                clickedChest.getInventory().setContents((sorter.sort().getContents()));
+            BlockState clickedBlockState = event.getClickedBlock().getState();
+            SortType sortType = playerSortType.get(playerName);
+            // chest click
+            if (clickedBlockState instanceof Chest) {
+                Chest clickedChest = (Chest) clickedBlockState;
+                Inventory chestInventory = clickedChest.getInventory();
+                Sortable sorter = InventoryUtil.getSorter(sortType, chestInventory);
+                chestInventory.setContents((sorter.sort().getContents()));
 
                 event.getPlayer().sendMessage(ChatColor.AQUA + "Chest contents have been sorted!");
+
+            // shulker box click
+            } else if (clickedBlockState instanceof ShulkerBox) { 
+                ShulkerBox clickedShulkerBox = (ShulkerBox) clickedBlockState;
+                Inventory shulkerBoxInventory = clickedShulkerBox.getInventory();
+                Sortable sorter = InventoryUtil.getSorter(sortType, shulkerBoxInventory);
+                shulkerBoxInventory.setContents((sorter.sort().getContents()));
+
+                event.getPlayer().sendMessage(ChatColor.AQUA + "ShulkerBox contents have been sorted!");
 
             } else {
                 // player clicked non chest block
